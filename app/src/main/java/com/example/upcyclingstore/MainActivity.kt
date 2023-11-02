fd7befd9dcb3f4df4727b49b853c5710a488717f
zcxzxc <zcxzxc@github.com>
2023-11-02 16:20:47 +0900

    Initial commit

---
+++ app/src/main/java/com/example/upcyclingstore/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.upcyclingstore
+
+import android.content.Intent
+import android.database.DatabaseUtils
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.upcyclingstore.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var b : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        b = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        b.btnMainLogin.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
